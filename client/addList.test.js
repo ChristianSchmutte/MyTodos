4bd6c167d61a677dff27f1ea0523678dbfb46ea1
Alexander Genkin <alexander_genkin@gmx.net>
2021-05-04 13:26:49 +0200

    test: add list test skeleton

---
+++ client/addList.test.js
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react'
+import fireEvent from '@testing-library/fire-event';
+
+import App from './App';
+
+test('show add task', () => {
+   render(<App/>);
+   const input = screen.getByLabelText('addList');
+
+   // assert
+});
+
+jest.mock('../path', () => ({
+  addTask: () => ({list: 'testList'})
+}));
+
+it ('should write task', async () => {
+  const input = screen.getByLabelText('addList');
+  userEvent.type(input, 'testList');
+  await fireEvent.keyDown(domNode, { key: 'Enter', code: 'Enter' });
+  expect(input).toHaveBeenCalledWith('testList');
+});
+
+it ('should add task', async () => {
+
+});
